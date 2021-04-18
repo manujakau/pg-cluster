a7c1f538cdfa0cdcae23b66ab64202115419020e
manujakau <manujakau@gmail.com>
2021-04-18 12:33:23 +0300

    add sg outputs

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,15 @@ output "private_subnet_a" {
 output "private_subnet_b" {
   value = module.networking.private_subnet_b
 }
+
+output "bastion_security_group" {
+  value = module.networking.bastion_security_group
+}
+
+output "application_security_group" {
+  value = module.networking.application_security_group
+}
+
+output "db_security_group" {
+  value = module.networking.db_security_group
+}
