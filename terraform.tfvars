4d7d8f71d9735ab8aaa3041673eacd566f9deb16
manujakau <manujakau@gmail.com>
2021-04-18 11:20:45 +0300

    default vars

---
+++ terraform.tfvars
@@ -0,0 +1,19 @@
+profile = "default"
+
+aws_region = "eu-north-1"
+
+vpc_cidr = "10.0.0.0/20"
+
+public_cidr = "10.0.1.0/28"
+
+private_cidr_1 = "10.0.2.0/28"
+
+private_cidr_2 = "10.0.3.0/28"
+
+accessip = "0.0.0.0/0"
+
+key_name = "WP"
+
+instances_type01 = "t3.micro"
+
+instances_type02 = "t3.small"
---
+++ variables.tf
@@ -0,0 +1,24 @@
+
+variable "profile" {}
+
+variable "aws_region" {}
+
+#networking variables
+
+variable "vpc_cidr" {}
+
+variable "public_cidr" {}
+
+variable "private_cidr_1" {}
+
+variable "private_cidr_2" {}
+
+variable "accessip" {}
+
+#compute variables
+
+variable "key_name" {}
+
+variable "instances_type01" {}
+
+variable "instances_type02" {}
