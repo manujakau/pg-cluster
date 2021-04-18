1f54ca7e1a9fa9a0666fab6006ff3f6629ef28dc
manujakau <manujakau@gmail.com>
2021-04-18 12:32:42 +0300

    add sg outputs

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -13,3 +13,15 @@ output "private_subnet_a" {
 output "private_subnet_b" {
   value = aws_subnet.pg_private_subnet_b.id
 }
+
+output "bastion_security_group" {
+  value = aws_security_group.pg_bastion_sg.id
+}
+
+output "application_security_group" {
+  value = aws_security_group.pg_app_sg.id
+}
+
+output "db_security_group" {
+  value = aws_security_group.pg_db_sg.id
+}
