102cf670e7759fd797cc6de62cc0ed5020db6c50
manujakau <manujakau@gmail.com>
2021-04-18 13:57:51 +0300

    db sys prep

---
+++ scripts/pg_master.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+hostnamectl set-hostname pgmaster
+
+sudo apt-get -y update
+sudo apt-get -y install nano wget
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+apt-get update -y
+apt-get -y install postgresql-13
+sudo /etc/init.d/postgresql start
+sudo systemctl is-enabled postgresql
+
+sudo -u postgres psql << EOF
+CREATE USER replication REPLICATION LOGIN CONNECTION LIMIT 1 ENCRYPTED PASSWORD 'pg1234test';
+EOF
+
+sudo -u postgres psql << EOF
+ALTER ROLE replication CONNECTION LIMIT -1;
+EOF
+
+password="admin"
+sudo useradd ansadmin
+yes $password | sudo passwd ansadmin
+sudo usermod -aG postgres ansadmin
+sudo echo "ansadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sed -i "s/PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+sudo systemctl reload sshd
---
+++ scripts/pg_slave.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+hostnamectl set-hostname pgslave
+
+sudo apt-get -y update
+sudo apt-get -y install nano wget
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+apt-get update -y
+apt-get -y install postgresql-13
+sudo /etc/init.d/postgresql start
+sudo systemctl is-enabled postgresql
+sudo /etc/init.d/postgresql stop
+
+password="admin"
+sudo useradd ansadmin
+yes $password | sudo passwd ansadmin
+sudo usermod -aG postgres ansadmin
+sudo echo "ansadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sed -i "s/PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+sudo systemctl reload sshd
