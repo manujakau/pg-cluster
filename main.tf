a0804c2cd40824d2f25bf41116aa87ab7ae71045
manujakau <manujakau@gmail.com>
2021-04-18 12:36:41 +0300

    add comput module

--- main.tf
+++ main.tf
@@ -10,3 +10,18 @@ module "networking" {
   subnet_cidrs = var.subnet_cidrs
   accessip     = var.accessip
 }
+
+# Deploy Compute Resources
+module "compute" {
+  source             = "./compute"
+  region             = var.aws_region
+  key_name           = var.key_name
+  instance_type_1    = var.instances_type01
+  instance_type_2    = var.instances_type02
+  public_subnets_a   = module.networking.public_subnet_a
+  private_subnet_a   = module.networking.private_subnet_a
+  private_subnet_b   = module.networking.private_subnet_b
+  ssh_security_group = module.networking.bastion_security_group
+  app_security_group = module.networking.application_security_group
+  db_security_group  = module.networking.db_security_group
+}
