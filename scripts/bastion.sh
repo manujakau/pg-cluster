0f204a096f2675f04731e1d889d61bedb9fabf81
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2021-04-19 16:37:58 +0300

    ansible prep

--- scripts/bastion.sh
+++ scripts/bastion.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 hostnamectl set-hostname bastion
-yum update -y && yum upgrade -yum
+yum update -y && yum upgrade -y
+amazon-linux-extras install ansible2 -y
+
+password="admin"
+sudo useradd ansadmin
+yes $password | sudo passwd ansadmin
+sudo usermod -aG postgres ansadmin
+sudo echo "ansadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo mkdir /home/ansadmin
+sudo usermod --shell /bin/bash --home /home/ansadmin ansadmin
+sudo chown -R ansadmin:ansadmin /home/ansadmin
+sed -i "s/PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+sudo systemctl reload sshd
 yum autoremove -y
