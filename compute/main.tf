ff86a6e4e595a644d5bc87b9b58cc51209816749
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2021-04-19 09:45:58 +0300

    add application host

--- compute/main.tf
+++ compute/main.tf
@@ -46,14 +46,25 @@ resource "aws_instance" "bastion_host" {
   user_data              = data.template_cloudinit_config.cloudinit-bastion.rendered
 }
 
+resource "aws_instance" "application_host" {
+  instance_type        = var.instance_type_2
+  ami                  = data.aws_ami.server_ami_2.id
+  iam_instance_profile = aws_iam_instance_profile.pg_cluster_profile.name
+
+  tags = {
+    Name = "application_host"
+  }
+
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.app_security_group]
+  subnet_id              = var.private_subnet_a
+  user_data              = data.template_cloudinit_config.cloudinit-application.rendered
+}
 
 resource "aws_instance" "db_master_host" {
   instance_type        = var.instance_type_2
   ami                  = data.aws_ami.server_ami_2.id
   iam_instance_profile = aws_iam_instance_profile.pg_cluster_profile.name
-  depends_on = [
-    aws_instance.db_slave_host,
-  ]
 
   tags = {
     Name = "db_master_host"
