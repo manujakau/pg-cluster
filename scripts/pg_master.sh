6832b2cf522b826487e0d00092ab2714fb283fc2
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2021-04-19 10:53:32 +0300

    prep user

--- scripts/pg_master.sh
+++ scripts/pg_master.sh
@@ -26,6 +26,6 @@ sudo echo "ansadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sudo mkdir /home/ansadmin
 sudo usermod --shell /bin/bash --home /home/ansadmin ansadmin
 sudo chown -R ansadmin:ansadmin /home/ansadmin
-sudo cp /etc/skel/.* /home/ansadmin/
+#sudo cp /etc/skel/.* /home/ansadmin/
 sed -i "s/PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sudo systemctl reload sshd
