89f0f4554183e2edacb9f38ba0995dc4b138ac5a
manujakau <manujakau@gmail.com>
2021-05-11 19:31:18 +0300

    change logic

--- test/pg_cluster_test.go
+++ test/pg_cluster_test.go
@@ -10,19 +10,24 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestTerraformAwsHelloWorldExample(t *testing.T) {
+func TestTerraformAWSinfra(t *testing.T) {
 	t.Parallel()
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "/home/manujak/pg-cluster/infra",
+		// The path to where our Terraform code is located
+		TerraformDir: "../infra",
 	})
 
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
+	// Run `terraform output` to get the values of output variables
 	publicIp := terraform.Output(t, terraformOptions, "app_public_ip")
 
+	// Verify the resource group exists
 	url := fmt.Sprintf("http://%s", publicIp)
-	http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World!", 30, 5*time.Second)
+	http_helper.HttpGetWithRetry(t, url, nil, 200, "<!doctype html>", 30, 5*time.Second)
 }
