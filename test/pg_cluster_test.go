2c9b88ac4f987ba46f0e01ca2e5c54837dca9fd5
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2021-05-12 08:49:24 +0300

    prep

--- test/pg_cluster_test.go
+++ test/pg_cluster_test.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
-
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
@@ -14,20 +13,15 @@ func TestTerraformAWSinfra(t *testing.T) {
 	t.Parallel()
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		// The path to where our Terraform code is located
 		TerraformDir: "../infra",
 	})
 
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the values of output variables
 	publicIp := terraform.Output(t, terraformOptions, "app_public_ip")
 
-	// Verify the resource group exists
 	url := fmt.Sprintf("http://%s/index.php?n=2", publicIp)
 	http_helper.HttpGetWithRetry(t, url, nil, 200, "4", 30, 5*time.Second)
 }
